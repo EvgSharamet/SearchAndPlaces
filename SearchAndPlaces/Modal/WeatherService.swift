3550c68dec96006f68f6e05ebb33b65affece798
Ruslan K <mr.sn8p@gmail.com>
2021-12-06 11:35:03 +0200

    Fix service structure

--- SearchAndPlaces/Modal/WeatherService.swift
+++ SearchAndPlaces/Modal/WeatherService.swift
@@ -54,7 +54,7 @@ class WeatherService {
         let byHours: [Int: WeatherInfo]
     }
     
-    typealias RequestResult = Result<WeatherData,Error>
+    typealias RequestResult = Result<WeatherData, Swift.Error>
     typealias RequestResultHandler = (RequestResult) -> Void
     
     //MARK: - data
@@ -63,47 +63,48 @@ class WeatherService {
     
     //MARK: - public functions
     
-    func requestWeatherOf(place: String, handler: RequestResultHandler) {
-        coordinatesToJson(place: place)
-    }
-    
-    func coordinatesToJson(place: String) {
+    func requestWeatherOf(place: String, handler: @escaping RequestResultHandler) {
         getCoordinateFrom(address: place ) { (location, error) in
             guard let location = location else {
+                handler(.failure(Error(info: "no location")))
                 return
             }
-
-            do{
-                try self.jsonToResponse(location)
+            do {
+                let weatherData = try self.jsonToResponse(location)
+                handler(.success(weatherData))
             } catch {
-                print( "Error here")
+                handler(.failure(error))
             }
         }
     }
     
-    func jsonToResponse(_ location:CLLocationCoordinate2D ) throws {
-        
+    //MARK: - private functions
+    
+    private func jsonToResponse(_ location:CLLocationCoordinate2D) throws -> WeatherData {
         let decoder = JSONDecoder()
         guard let url = URL( string: "https://api.openweathermap.org/data/2.5/onecall?lat=\(location.latitude)&lon=\(location.longitude)&lang=ru&exclude=minutely,daily&units=metric&appid=167ec7c4487c8b004df1c9b138fb6600")
         else {
-            return
+            throw Error(info: "can't get url")
         }
 
         guard let jsonString = try? String(contentsOf: url, encoding:.utf8) else {
-            return
+            throw Error(info: "can't decode url")
         }
     
         let jsonData = Data(jsonString.utf8)
         guard let answer = try? decoder.decode(Response.self, from: jsonData) else {
-            return
+            throw Error(info: "can't decode Response")
         }
+        let info = WeatherInfo(icon: answer.hourly.first?.icon ?? "",
+                               description: "",
+                               time: "",
+                               temperature: Int(answer.hourly.first?.temp ?? 30))
+        return WeatherData(now: info, byHours: [:])
     }
     
-    func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Swift.Error?) -> () ) {
+    private func getCoordinateFrom(address: String, completion: @escaping(_ coordinate: CLLocationCoordinate2D?, _ error: Swift.Error?) -> () ) {
         CLGeocoder().geocodeAddressString(address) { completion($0?.first?.location?.coordinate, $1) }
     }
-    
-    //MARK: - private functions
     
     private init() {}
 }
